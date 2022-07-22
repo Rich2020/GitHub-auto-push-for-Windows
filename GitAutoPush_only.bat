3ea0c3f4da036c26373a5246adb85b328c7c2f9c
woodgreat <38380995+woodgreat@users.noreply.github.com>
2022-07-22 08:36:08 +0800

    add only push action bat file
    you can use it in your git repo dir ,or edit it as normal usage !

---
+++ GitAutoPush_only.bat
@@ -0,0 +1,26 @@
+:loop
+	
+	:: Navigate to the directory you wish to push to GitHub
+	::Change <path> as needed. Eg. C:\Users\rich\Desktop\Writings
+	::cd <path>
+
+	
+	::Add all files in the directory
+	git add --all
+	
+	::Commit all changes with the message "auto push". 
+	::Change as needed.
+	git commit -m "auto push"
+	
+	::Push all changes to GitHub 
+	git push
+	
+	::Alert user to script completion and relaunch.
+	echo Complete. Relaunching...
+	
+	::Wait 300 seconds until going to the start of the loop.
+	::Change as needed.
+	TIMEOUT 300
+	
+::Restart from the top.	
+goto loop
