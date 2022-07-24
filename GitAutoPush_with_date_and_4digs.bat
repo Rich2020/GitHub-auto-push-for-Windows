abcc852f2e5652453ec19a5b7db935e7748e509b
z5 <z5@miniasm.com>
2022-07-24 16:27:34 +0800

    add new auto save .bat file ,with date and 4 digital numbers

--- GitAutoPush_with_date_and_4digs.bat
+++ GitAutoPush_with_date_and_4digs.bat
@@ -1,10 +1,9 @@
 :loop
-
+	
   :: ! place this file to your working directory , first ! and run it keet it running ,It will works well !
   ::  2022.07.24.wood
 
 
-	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. C:\Users\rich\Desktop\Writings
 	::cd <path>
@@ -15,7 +14,16 @@
 	
 	::Commit all changes with the message "auto push". 
 	::Change as needed.
-	git commit -m "auto push"
+	::git commit -m "auto push"
+
+  set year=%date:~0,4%
+  set month=%date:~5,2%
+  set day=%date:~8,2%
+
+  ::commit add 4 digitals random number
+  set /a num=%Random% %%9000 +1000 
+
+  git commit -m "auto push %year%%month%%day%_%num%"
 	
 	::Push all changes to GitHub 
 	git push
@@ -25,7 +33,7 @@
 	
 	::Wait 300 seconds until going to the start of the loop.
 	::Change as needed.
-	TIMEOUT 300
+	TIMEOUT 2000
 	
 ::Restart from the top.	
 goto loop
